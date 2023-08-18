c7ccb067bc2412d8045aa1ca3fcecd3d1828bd22
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-08-18 20:15:55 +0530

    fixed a installation bug

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Pyutils',
     version='0.1.0',
-    packages=['Pyutils'],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     license='MIT',
+    author='Jeel Dobariya',
     description='A collection of utility modules for Python development',
     url='https://github.com/JeelDobariya38/Pyutils'
 )
