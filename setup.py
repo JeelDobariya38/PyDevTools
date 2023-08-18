9dd047ed16722977178dc212ef3ab8e7f962db80
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-08-18 19:34:28 +0530

    add a setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Pyutils',
+    version='0.1.0',
+    packages=['Pyutils'],
+    license='MIT',
+    description='A collection of utility modules for Python development',
+    url='https://github.com/JeelDobariya38/Pyutils'
+)
