c3b30f7d5475cb562068cb15643c396770919964
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-06 19:12:12 +0530

    upgraded the file structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pydevtools',
     version='0.1.0',
-    packages=find_packages(where="pydevtools"),
+    packages=find_packages(),
     install_requires=[
         'colorama',
     ],
