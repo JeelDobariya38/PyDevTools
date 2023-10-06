c0e34a3919d82691fec299cf12b42716ef983525
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-06 16:58:37 +0530

    made name of function a little shorter

--- pydevtools/math/math.py
+++ pydevtools/math/math.py
@@ -4,7 +4,7 @@ def add(*nums: int | float) -> int | float:
         total = total + num
     return total
 
-def subtract(*nums: int | float) -> int | float:
+def sub(*nums: int | float) -> int | float:
     if len(nums) != 0:
         total = nums[0]
         for num in nums[1:]:
@@ -13,14 +13,14 @@ def subtract(*nums: int | float) -> int | float:
     else:
         return 0
 
-def multiply(*nums: int | float) -> int | float:
+def mult(*nums: int | float) -> int | float:
 
     total = 1
     for num in nums:
         total = total * num
     return total
 
-def divide(*nums: int | float) -> int | float:
+def div(*nums: int | float) -> int | float:
     if len(nums) != 0:
         total = nums[0] * nums[0]
         for num in nums:
