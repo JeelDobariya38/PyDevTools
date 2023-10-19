c9f7623df854daab5a0ef5ff3fcd301fc65feb50
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-19 21:38:46 +0530

    add some extra property based test and fixes the file name error

--- Tests/property_based/test_math_property.py
+++ Tests/property_based/test_math_property.py
@@ -24,3 +24,24 @@ def test_subtraction_poperty():
         assert math.sub(a, 0) == a # a - 0 = a
         assert math.sub(math.sub(a, b), c) != math.sub(a, math.sub(b,c)) # (a - b) - c = a - (b - c)
         assert a * math.sub(b, c) == math.sub(a*b, a*c) # a * (b - c) = a*b - a*c
+
+def test_multiplication_poperty():
+    for i in range(100):
+        a = randint(1,999)
+        b = randint(1,999)
+        c = randint(1,999)
+
+        assert math.mult(a, b) == math.mult(b, a) # a * b = b * a
+        assert math.mult(a,(-1 * a)) != -2*a # a * -1 * a = -2 * a
+        assert math.mult(a, 0) == 0 # a * 0 = 0
+        assert math.mult(math.mult(a, b), c) == math.mult(a, math.mult(b,c)) # (a * b) * c = a * (b * c)
+
+def test_division_poperty():
+    for i in range(100):
+        a = randint(1,999)
+        b = randint(1,999)
+        c = randint(1,999)
+
+        assert math.div(a, b) != math.div(b, a) # a / b != b / a
+        assert math.div(a,(-1 * a)) != -1 # a / -1 / a =  -1
+        assert math.div(math.div(a, b), c) != math.div(a, math.div(b,c)) # (a / b) / c = a / (b / c)
