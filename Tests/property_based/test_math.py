76bf4a8de41dab4f8d1fab4694082e870bc802f9
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-10-19 21:26:58 +0530

    add property based testing for addition and subtraction

---
+++ Tests/property_based/test_math.py
@@ -0,0 +1,26 @@
+from pydevtools.math import math
+from random import randrange, randint
+
+def test_addition_poperty():
+    for i in range(100):
+        a = randint(1,999)
+        b = randint(1,999)
+        c = randint(1,999)
+
+        assert math.add(a, b) == math.sub(b, a) # a + b = b + a
+        assert math.add(a,(-1 * a)) == 0 # a + (-a) = 0
+        assert math.add(a, 0) == a # a + 0 = a
+        assert math.add(math.add(a, b), c) == math.add(a, math.add(b,c)) # (a + b) + c = a + (b + c)
+        assert a * math.add(b, c) == math.add(a*b, a*c) # a * (b + c) = a*b + a*c
+
+def test_subtraction_poperty():
+    for i in range(100):
+        a = randint(1,999)
+        b = randint(1,999)
+        c = randint(1,999)
+
+        assert math.sub(a, b) != math.sub(b, a) # a - b != b - a
+        assert math.sub(a,(-1 * a)) != 2*a # a - (-a) = 2(a)
+        assert math.sub(a, 0) == a # a - 0 = a
+        assert math.sub(math.sub(a, b), c) != math.sub(a, math.sub(b,c)) # (a - b) - c = a - (b - c)
+        assert a * math.sub(b, c) == math.sub(a*b, a*c) # a * (b - c) = a*b - a*c
