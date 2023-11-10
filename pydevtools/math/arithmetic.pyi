b886ebba4277471c9441c5ee78f2b8a4cd7f1b1b
Jeel Dobariya <jeeldobariya38@gmail.com>
2023-11-10 15:58:42 +0530

    add some arithmetic function

---
+++ pydevtools/math/arithmetic.pyi
@@ -0,0 +1,125 @@
+def add(a: int | float, b: int | float) -> int | float:
+    """
+    Add two numbers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The result of the addition.
+
+    Example:
+        >>> add(5, 3)
+        8.0
+    """
+
+def sub(a: int | float, b: int | float) -> int | float:
+    """
+    Subtract one number from another.
+
+    Args:
+        a (int or float): The number to subtract from.
+        b (int or float): The number to subtract.
+
+    Returns:
+        int or float: The result of the subtraction.
+
+    Example:
+        >>> sub(8, 3)
+        5.0
+    """
+
+def mul(a: int | float, b: int | float) -> int | float:
+    """
+    Multiply two numbers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The result of the multiplication.
+
+    Example:
+        >>> mul(4, 2)
+        8.0
+    """
+
+def div(a: int | float, b: int | float) -> int | float:
+    """
+    Divide one number by another.
+
+    Args:
+        a (int or float): The dividend.
+        b (int or float): The divisor.
+
+    Returns:
+        int or float: The result of the division.
+
+    Example:
+        >>> div(10, 2)
+        5.0
+    """
+
+def rem(a: int | float, b: int | float) -> int | float:
+    """
+    Calculate the remainder when dividing one number by another.
+
+    Args:
+        a (int or float): The dividend.
+        b (int or float): The divisor.
+
+    Returns:
+        int or float: The remainder of the division.
+
+    Example:
+        >>> rem(10, 3)
+        1.0
+    """
+
+def pow(a: int | float, b: int | float) -> int | float:
+    """
+    Calculate the result of raising one number to the power of another.
+
+    Args:
+        a (int or float): The base.
+        b (int or float): The exponent.
+
+    Returns:
+        int or float: The result of the exponentiation.
+
+    Example:
+        >>> pow(2, 3)
+        8.0
+    """
+
+def sqrt(a: int | float) -> int | float:
+    """
+    Calculate the square root of a number.
+
+    Args:
+        a (int or float): The number for which to calculate the square root.
+
+    Returns:
+        int or float: The square root of the input number.
+
+    Example:
+        >>> sqrt(16)
+        4.0
+    """
+
+def cbrt(a: int | float) -> int | float:
+    """
+    Calculate the cube root of a number.
+
+    Args:
+        a (int or float): The number for which to calculate the cube root.
+
+    Returns:
+        int or float: The cube root of the input number.
+
+    Example:
+        >>> cbrt(27)
+        3.0
+    """
